292a207751f22d6785ce9bfbddb18ff429c59943
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2022-03-28 10:16:53 +0200

    Letters generated &  mapped to row&Col

---
+++ src/SelectableAreas.js
@@ -0,0 +1,165 @@
+import SelectionArea, { SelectionEvent } from "@viselect/react";
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function SelectableAreas() {
+  const [selected, setSelected] = useState(() => new Set());
+  const [column, setColumn] = useState([]);
+  const [row, setRow] = useState([]);
+
+  // -----generating letters---
+
+  function colName(n) {
+    var codeA = "a".charCodeAt(0);
+    var codeZ = "z".charCodeAt(0);
+    var len = codeZ - codeA + 1;
+
+    var s = "";
+    while (n >= 0) {
+      s = String.fromCharCode((n % len) + codeA) + s;
+      n = Math.floor(n / len) - 1;
+    }
+
+    return s.toUpperCase();
+  }
+  function rowName(n) {
+    var codeA = "a".charCodeAt(0);
+    var codeZ = "z".charCodeAt(0);
+    var len = codeZ - codeA + 1;
+
+    const box = new Array(5)
+      .fill(0)
+      .map((_, index) => (
+        <div
+          className={selected.has(index) ? "selected selectable" : "selectable"}
+          data-key={index}
+          key={index}
+        />
+      ));
+
+    var s = "";
+    while (n >= 0) {
+      s = String.fromCharCode((n % len) + codeA) + s;
+      n = Math.floor(n / len) - 1;
+    }
+
+    return s.toUpperCase();
+    // return s.toUpperCase() + box;
+  }
+
+  useEffect(() => {
+    for (let n = 0; n < 50; n++) {
+      console.log("UseEffcet Column Loop");
+      setColumn((prVal) => {
+        return [...prVal, colName(n)];
+      });
+      // console.log(colName(n));
+    }
+    for (let n = 0; n < 200; n++) {
+      console.log("UseEffcet row Loop");
+      setRow((prVal) => {
+        return [...prVal, rowName(n)];
+      });
+      // console.log(setRow(n));
+    }
+  }, []);
+
+  // console.log(column);
+  // console.log(row);
+
+  // ------
+
+  const extractIds = (els) =>
+    els
+      .map((v) => v.getAttribute("data-key"))
+      .filter(Boolean)
+      .map(Number);
+
+  const onStart = ({ event, selection }) => {
+    if (!event?.ctrlKey && !event?.metaKey) {
+      selection.clearSelection();
+      setSelected(() => new Set());
+    }
+  };
+
+  const onMove = ({
+    store: {
+      changed: { added, removed },
+    },
+  }) => {
+    setSelected((prev) => {
+      const next = new Set(prev);
+      extractIds(added).forEach((id) => next.add(id));
+      extractIds(removed).forEach((id) => next.delete(id));
+      return next;
+    });
+  };
+
+  return (
+    <SelectionArea
+      className="container"
+      onStart={onStart}
+      onMove={onMove}
+      selectables=".selectable"
+    >
+      <Table>
+        <TableHead>
+          <TableRow className="TableCell">
+            <TableCell></TableCell>
+            {column &&
+              column.map((col, i) => {
+                return (
+                  <>
+                    <TableCell
+                      key={i}
+                      data-key={i}
+                      className={
+                        selected.has(i) ? "selected selectable" : "selectable"
+                      }
+                    >
+                      {col}
+                    </TableCell>
+                  </>
+                );
+              })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* <TableRow></TableRow> */}
+
+          {row &&
+            row.map((r, i) => {
+              return (
+                <TableRow>
+                  <TableCell
+                    key={i}
+                    data-key={i}
+                    className={
+                      selected.has(i) ? "selected selectable" : "selectable"
+                    }
+                  >
+                    {r}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+      {/* {new Array(400).fill(0).map((_, index) => (
+        <div
+          className={selected.has(index) ? "selected selectable" : "selectable"}
+          data-key={index}
+          key={index}
+        />
+      ))} */}
+    </SelectionArea>
+  );
+}
+
+export default SelectableAreas;
