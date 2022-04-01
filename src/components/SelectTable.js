bc2f90b558e64995c787e34b9b79d377badce9ae
Ranjeet Kumar <ranjeetkumar2311@gmail.com>
2022-04-01 07:28:46 +0200

    basic styles Added

--- src/components/SelectTable.js
+++ src/components/SelectTable.js
@@ -5,14 +5,37 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import letterGenerator from "./lettersGenerator";
-import { makeStyles } from "@material-ui/core/styles";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { withStyles } from "@material-ui/core/styles";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.common.black,
+    padding: " 10px 50px",
+  },
+  body: {
+    fontSize: 12,
+    width: "20px",
+    height: " 10px",
+    padding: " 10px 50px",
+    border: "1px solid grey",
+  },
+}))(TableCell);
 
 function SelectTable({ selected }) {
   const [column, setColumn] = useState([]);
   const [row, setRow] = useState([]);
 
-  const tableCells = new Array(50);
+  const tableRowCells = new Array(50);
+  const boxes = tableRowCells
+    .fill(0)
+    .map((_, index) => (
+      <StyledTableCell
+        className={selected.has(index) ? "selected selectable" : "selectable"}
+        data-key={index}
+        key={index}
+      ></StyledTableCell>
+    ));
 
   useEffect(() => {
     for (let n = 0; n < 50; n++) {
@@ -31,10 +54,10 @@ function SelectTable({ selected }) {
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell></TableCell>
+          <StyledTableCell></StyledTableCell>
           {column &&
             column.map((col, i) => {
-              return <TableCell key={i}>{col}</TableCell>;
+              return <StyledTableCell key={i}>{col}</StyledTableCell>;
             })}
         </TableRow>
       </TableHead>
@@ -42,17 +65,9 @@ function SelectTable({ selected }) {
         {row &&
           row.map((r, i) => {
             return (
-              <TableRow>
-                <TableCell key={i}>{r}</TableCell>
-                {tableCells.fill(0).map((_, index) => (
-                  <TableCell
-                    className={
-                      selected.has(index) ? "selected selectable" : "selectable"
-                    }
-                    data-key={index}
-                    key={index}
-                  ></TableCell>
-                ))}
+              <TableRow key={i}>
+                <StyledTableCell>{r}</StyledTableCell>
+                {boxes}
               </TableRow>
             );
           })}
